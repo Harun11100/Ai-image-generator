d2e1bd5a2361e73656ee5801949f2ff421c76e03
Harun11100 <145543162+Harun11100@users.noreply.github.com>
2024-10-30 18:33:35 +0600

    Routing part is complete

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,8 +1,39 @@
-
+import { BrowserRouter, Route, Router, Routes,Link} from "react-router-dom"
+import { CreatePost, Home } from "./Pages"
+import {logo} from "./assets"
 const App = () => {
   return (
-    <div className="text-5xl">App</div>
-  )
+    <BrowserRouter >
+    <header className="w-full flex justify-between items-center bg-white px-10 py-4 border-b border-b-[
+    #e6ebf4]">
+      <Link to='/'>
+      <img src={logo} alt="logo" className="w-28 object-contain" />
+      </Link>
+      <Link to='/create-post' className="font-inter font-medium bg-[#6469ff] text-white px-4 py-2 rounded-md">
+      Create
+      </Link>
+
+      
+     
+    </header>
+    <main className="sm:p-8  px-4 py-8 w-full bg-[#f9f8f]
+    min-h-[calc(100vh-73px)]" >
+      <Routes>
+
+      <Route path="/" element={<Home/>} />
+      <Route path="create-post" element={<CreatePost/>} />
+
+      </Routes>
+
+
+
+    </main>
+    </BrowserRouter>
+     
+     
+
+ 
+  ) 
 }
 
 export default App
---
+++ Client/src/Pages/index.js
@@ -0,0 +1,11 @@
+
+//this will work as an exporter 
+
+import Home from "./Home";
+import CreatePost from "./CreatePost";
+
+
+export{
+      Home,
+      CreatePost
+}
